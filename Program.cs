a6e5dc495c8e12ceed2ab6a187dbc304131f4276
ChristianWewer <43784267+ChristianWewer@users.noreply.github.com>
2020-05-20 15:03:01 +0200

    Added functionality to get index
    The index is now also returned in the struct ResultLists

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 class NChooseCrit
 {
+    public struct Result
+    {
+        public List<double> resultSet;
+        public List<int> resultIndexSet;
+    }
+    public struct ResultLists
+    {
+        public List<List<double>> resultSets;
+        public List<List<int>> resultIndexSets;
+    }
+
     // Function to print the subsets whose 
     // sum is equal to the given target K 
-    public static List<double> sumSubsets(
+    public static Result sumSubsets(
         double[] set, int n, double target)
     {
         // Create the new array with size 
@@ -14,6 +25,7 @@ class NChooseCrit
         int[] x = new int[set.Length];
         int j = set.Length - 1;
         List<double> resultSet = new List<double>();
+        List<int> resultIndexSet = new List<int>();
 
         // Convert the array into binary array 
         while (n > 0)
@@ -37,30 +49,50 @@ class NChooseCrit
             for (int i = 0; i < set.Length; i++)
             {
                 if (x[i] == 1)
+                {
+                    // Values:
                     resultSet.Add(set[i]);
+                    // Index of values:
+                    resultIndexSet.Add(i);
+                }
+
             }
         }
-        return resultSet;
+        var result = new Result
+        {
+            resultSet = resultSet,
+            resultIndexSet = resultIndexSet
+        };
+        return result;
     }
 
     // Function to find the subsets with sum K 
-    public static List<List<double>> findSubsets(double[] arr, int K)
+    public static ResultLists findSubsets(double[] arr, double K)
     {
         // Calculate the total no. of subsets 
         int x = (int)Math.Pow(2, arr.Length);
-        Console.WriteLine("here we are!");
         // Run loop till total no. of subsets 
         // and call the function for each subset 
         List<List<double>> resultSets = new List<List<double>>();
+        List<List<int>> resultIndexSets = new List<List<int>>();
         List<double> tmp_list = new List<double>();
+        List<int> tmp_index_list = new List<int>();
         for (int i = 1; i < x; i++)
-        { 
-            tmp_list = sumSubsets(arr, i, K);
+        {
+            Result test = sumSubsets(arr, i, K);
+            tmp_list = test.resultSet;
+            tmp_index_list = test.resultIndexSet;
             if (tmp_list.Count != 0) { 
                 resultSets.Add(tmp_list);
+                resultIndexSets.Add(tmp_index_list);
             }
         }
-        return resultSets;
+        var result = new ResultLists
+        {
+            resultSets = resultSets,
+            resultIndexSets = resultIndexSets
+        };
+        return result;
     }
 
     public static void printResult(List<List<double>> results)
@@ -69,7 +101,19 @@ class NChooseCrit
         {
             foreach (double number in element)
             {
-                Console.Write(number + ",");
+                Console.Write(number + " - ");
+            }
+            Console.WriteLine("");
+        }
+    }
+
+    public static void printResultIndex(List<List<int>> results)
+    {
+        foreach (List<int> element in results)
+        {
+            foreach (int number in element)
+            {
+                Console.Write(number + " - ");
             }
             Console.WriteLine("");
         }
@@ -78,14 +122,18 @@ class NChooseCrit
     public static void Main(String[] args)
     {
         // Input array: 
-        double[] arr = { 5, 10, 12, 13, 15, 18 };
+        //double[] arr = { 1.2, 1.3, 1.2, 1.3, 1.4, 1.5, 1.2, 2, 0.8, 0.2, 0.5 };
+        double[] arr = { 1.2, 1.3, 1.4, 1.5, 2, 0.8, 0.2, 0.5 };
         // Goal:
-        int K = 30;
-        List<List<double>> results = new List<List<double>>();
-        results = findSubsets(arr, K);
+        double K = 3.2;
+        ResultLists results = findSubsets(arr, K);
+        List<List<double>> resultsvals = results.resultSets;
+        List<List<int>> resultIndexvals = results.resultIndexSets;
 
         // Print results to console for debugging:
-        printResult(results);
-
+        Console.WriteLine("Values");
+        printResult(resultsvals);
+        Console.WriteLine("Index");
+        printResultIndex(resultIndexvals);
     }
 }
